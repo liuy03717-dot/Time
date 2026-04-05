68d77f39a0c74a00945261c4de17e17eeccde42a
liuy03717-dot <liuy03717@gmail.com>
2026-04-05 11:45:08 +0800

    Update App.xaml.cs

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,15 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿namespace TimeApp;
 
-namespace TimeApp
+public partial class App : Application
 {
-    public partial class App : Application
+    public App()
     {
-        public App()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
+    }
 
-        protected override Window CreateWindow(IActivationState? activationState)
-        {
-            return new Window(new AppShell());
-        }
+    protected override Window CreateWindow(IActivationState? activationState)
+    {
+        //Make sure the package is within the NavigationPage.
+        return new Window(new NavigationPage(new MainPage()));
     }
 }
