f546dce4ba655937c71d826584929a0ae24116f8
liuy03717-dot <liuy03717@gmail.com>
2026-04-05 11:47:14 +0800

    Update MainPage.xaml.cs

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,42 +1,60 @@
-﻿using System.Collections.ObjectModel;
+using System;
+using System.Linq;
+using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
 
-namespace TimeApp;
-
-public partial class MainPage : ContentPage
+namespace TimeApp
 {
-    public ObservableCollection<EventItem> Events { get; set; }
-
-    public MainPage()
+    public partial class MainPage : ContentPage
     {
-        InitializeComponent();
+        public ObservableCollection<EventItem> Events { get; set; }
 
-        // 模拟数据
-        Events = new ObservableCollection<EventItem>
+        public MainPage()
         {
-            new EventItem { Title = "Mom's Birthday", Date = "Oct 24, 2026", DaysLeft = "5" },
-            new EventItem { Title = "Final Exam", Date = "Nov 15, 2026", DaysLeft = "27" }
-        };
+            InitializeComponent();
 
-        EventsList.ItemsSource = Events;
-    }
+            Events = new ObservableCollection<EventItem>
+            {
+                new EventItem { Title = "Mom's Birthday", TargetDate = new DateTime(DateTime.Now.Year, 10, 24) },
+                new EventItem { Title = "Final Exam", TargetDate = DateTime.Now.AddDays(27) }
+            };
 
-    // 点击卡片跳转
-    private async void OnEventTapped(object sender, EventArgs e)
-    {
-        var frame = sender as Frame;
-        var selectedEvent = frame?.BindingContext as EventItem;
+            EventsList.ItemsSource = Events;
+        }
 
-        if (selectedEvent != null)
+        // 1. Click on the blank area of the card to jump to the detail page.
+        private async void OnEventSelected(object sender, SelectionChangedEventArgs e)
         {
-            // 将选中的标题传递给下一页
-            await Navigation.PushAsync(new DetailPage(selectedEvent.Title));
+            if (e.CurrentSelection.FirstOrDefault() is EventItem selectedEvent)
+            {
+                await Navigation.PushAsync(new DetailPage(selectedEvent));
+                ((CollectionView)sender).SelectedItem = null;
+            }
+        }
+
+        // 2. Click the + button at the bottom right to navigate to the add page.
+        private async void OnAddEventClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new AddEventPage(Events));
+        }
+
+        // 3. Click the red "×" button on the card to trigger the deletion.
+        private async void OnExplicitDeleteClicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+
+            // Determine which event card the currently clicked button belongs to
+            if (button?.BindingContext is EventItem eventToDelete)
+            {
+                bool confirm = await DisplayAlert("Delete Event",
+                                                  $"Are you sure you want to delete '{eventToDelete.Title}'?",
+                                                  "Yes", "No");
+
+                if (confirm)
+                {
+                    Events.Remove(eventToDelete);
+                }
+            }
         }
     }
-}
-
-public class EventItem
-{
-    public string Title { get; set; }
-    public string Date { get; set; }
-    public string DaysLeft { get; set; }
 }
