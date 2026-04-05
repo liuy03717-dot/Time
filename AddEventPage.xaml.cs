49428765ae57d201c521b453d4556576281d0304
liuy03717-dot <liuy03717@gmail.com>
2026-04-05 11:38:47 +0800

    Add files via upload

---
+++ AddEventPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Microsoft.Maui.Controls;
+
+namespace TimeApp
+{
+    public partial class AddEventPage : ContentPage
+    {
+        private ObservableCollection<EventItem> _mainEvents;
+
+        public AddEventPage(ObservableCollection<EventItem> events)
+        {
+            InitializeComponent();
+            _mainEvents = events;
+            DatePicker.Date = DateTime.Today;
+        }
+
+        private async void OnSaveClicked(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TitleEntry.Text))
+            {
+                // Use ?? DateTime.Today to solve the nullable date error in the latest version of MAUI.
+                var newEvent = new EventItem
+                {
+                    Title = TitleEntry.Text,
+                    TargetDate = DatePicker.Date ?? DateTime.Today
+                };
+
+                _mainEvents.Add(newEvent);
+                await Navigation.PopAsync();
+            }
+            else
+            {
+                await DisplayAlert("Error", "Please enter an event name.", "OK");
+            }
+        }
+    }
+}
